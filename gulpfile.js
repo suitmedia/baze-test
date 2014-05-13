2f147cfdc13c9d151ea11d5ce0d53d7e2350494e
ImBobby <squidz.bee@gmail.com>
2014-05-13 11:21:44 +0700

    add dependencies

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+
+/* ------------------------------------------------------------------------
+
+    Baze Test
+    v0.1.0
+    Testing tool by Suitmedia
+
+   ------------------------------------------------------------------------ */
+
+var gulp        = require('gulp'),
+    gUtil       = require('gulp-util'),
+
+    // CSS Lint
+    cssLint     = require('gulp-csslint'),
+
+    // JS Hint
+    jsHint      = require('gulp-jshint'),
+
+    // Image min
+    imageMin    = require('gulp-imagemin');
+
+
+/* CSS Lint
+--------------------------------------------------------------------------- */
+
+gulp.task('css-lint', function () {
+    var options = {
+        'box-sizing'                : false,    // no longer support IE7 
+        'universal-selector'        : false,    // http://goo.gl/UbxPMq
+        'unique-headings'           : false,
+        'compatible-vendor-prefixes': false,    // handled by autoprefixer
+        'gradients'                 : false,    // handled by autoprefixer
+        'vendor-prefix'             : false,    // handled by autoprefixer
+        'box-model'                 : false,    // border-box specified
+        'known-properties'          : false
+    };
+
+    return gulp
+        .src('test/css/*.css')
+        .pipe(cssLint(options))
+        .pipe(cssLint.reporter());
+
+});
+
+
+/* JS Hint
+--------------------------------------------------------------------------- */
+
+gulp.task('js-hint', function () {
+
+    return gulp
+        .src('test/js/*.js')
+        .pipe(jsHint())
+        .pipe(jsHint.reporter());
+
+});
+
+
+/* Optimize image
+--------------------------------------------------------------------------- */
+
+gulp.task('image-min', function () {
+
+    return gulp
+        .src(['img/*.jpg', 'img/*.png', 'img/*.gif'])
+        .pipe(imageMin());
+
+});
